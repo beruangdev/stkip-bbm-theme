df6bec0ab1a561a87b0ec6ff56156ac7230412bf
beruangdev <beruangdev@gmail.com>
2023-07-11 20:52:45 +0700

    mm 3

--- inc/activated/post-edit-post-type.php
+++ inc/activated/post-edit-post-type.php
@@ -1,12 +1,12 @@
 <?php
-function edit_custom_post_type_asdqwqfw()
+function edit_custom_post_type_asdqwqfwwqe()
 {
   $obj = get_post_type_object('post');
   $obj->rewrite['slug'] = 'articles';
   $obj->has_archive = true;
   register_post_type('post', $obj);
 }
-add_action('init', 'edit_custom_post_type_asdqwqfw', 0);
+add_action('init', 'edit_custom_post_type_asdqwqfwwqe', 0);
 
 // function flush_rewrite_rules_on_post_type_registration()
 // {
